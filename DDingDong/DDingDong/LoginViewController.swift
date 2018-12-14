ec9c1e767fdc29dbbe7157befd2291b128e2af9e
byunsangjin <qustkdwls1@naver.com>
2018-12-15 02:21:56 +0900

    구글 로그인 구현

--- DDingDong/DDingDong/LoginViewController.swift
+++ DDingDong/DDingDong/LoginViewController.swift
@@ -7,10 +7,36 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 class LoginViewController: UIViewController {
-
+    
+    // MARK:- Outlets
+    @IBOutlet var signInButton: GIDSignInButton!
+    
+    
+    
+    // MARK:- Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        GIDSignIn.sharedInstance().uiDelegate = self // 구글 아이디 로그인 델리 게이트
+        
     }
+    
+    
+    
+    func signIn() {
+        let mainVC = self.storyboard?.instantiateViewController(withIdentifier: "MainViewController") as! MainViewController
+        
+        self.present(mainVC, animated: true)
+    }
+}
+
+
+
+// 구글 로그인 델리게이트
+extension LoginViewController: GIDSignInUIDelegate {
+    
 }
---
+++ DDingDong/DDingDong/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  DDingDong
+//
+//  Created by Byunsangjin on 15/12/2018.
+//  Copyright © 2018 Byunsangjin. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("main VC")
+    }
+
+}
